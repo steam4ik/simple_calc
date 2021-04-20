c5491836097dd274c89d42be7d17df51e779cf0e
steam4ik <61014983+steam4ik@users.noreply.github.com>
2021-04-20 12:21:11 +0500

    Add files via upload

---
+++ calcEng.py
@@ -0,0 +1,24 @@
+print("Calculator v1.0 Eng")
+
+a = float( input("Enter the first number: ") )
+b = float( input("Enter the second number: ") )
+
+znak = input:("Pick the sign +, - : ")
+print("You select: ", znak)
+
+if znak == "+":
+    c = a + b   
+    print("Result: ", c)
+
+elif znak == "-":
+    c = a - b
+    print("Result: ", c)
+    
+
+else:
+    print("Pick the right sign!")
+
+
+print("vk.com/whoisrockstar")
+
+input()
